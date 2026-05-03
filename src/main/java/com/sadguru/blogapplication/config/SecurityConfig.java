eba7fcbf6e8eff991e9ec102d40df25949615f0a
Delta Student <sadguru.patil04@gmail.com>
2026-05-03 17:14:12 +0530

    last commit

--- src/main/java/com/sadguru/blogapplication/config/SecurityConfig.java
+++ src/main/java/com/sadguru/blogapplication/config/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/api/posts/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/categories/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/users/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/users/").permitAll()
 
                         // ✅ AUTH APIs
                         .requestMatchers("/auth/**").permitAll()
